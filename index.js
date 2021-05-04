becca6b1cee1330e6b26694d58e77944fe71f131
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2021-05-04 13:04:02 +0200

    Determin movement

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: true,
-        width,
-        height
-    }
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: true,
+    width,
+    height,
+  },
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
@@ -21,23 +21,70 @@ Runner.run(Runner.create(), engine);
 // Walls
 
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),
-    Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),
-    Bodies.rectangle(0, height/2, 40, height, {isStatic: true}),
-    Bodies.rectangle(width, height/2, 40, height, {isStatic: true})
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
 // Maze generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
 const grid = Array(cells)
-.fill(null)
-.map(() => Array(cells).fill(false));
+  .fill(null)
+  .map(() => Array(cells).fill(false));
 
 const verticals = Array(cells)
-.fill(null)
-.map(() => Array(cells - 1).fill(false));
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
 
 const horizontals = Array(cells - 1)
-.fill(null)
-.map(() => Array(cells).fill(false));
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+  // Mark this cell as being visited
+  grid[row][column] = true;
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1],
+  ]);
+
+  // For each neighbor...
+  for (let neighbor of neighbors) {
+  // See if that neighbor is out of bounds
+    const [nextRow, nextColumn] = neighbor;
+
+  // If we have visited that neighbor, continue to next neighbor
+    if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+        
+    }
+  //Remove a wall from either horizontals or verticals
+  }
+  // Visit that next cell
+};
+stepThroughCell(startRow, startColumn);
